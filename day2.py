32334880b9522ed039107f95853d0fdb9b330235
dinesh2104 <52415525+dinesh2104@users.noreply.github.com>
2020-11-24 10:12:19 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,20 @@
+#1Q
+a=b=c=211
+print(a/10)
+print(b/50)
+print(c/60)
+
+#2Q
+str1="leave"
+new_str=str1[:2]+'G'+str1[3:]
+print(new_str)
+
+#3Q
+a1=10
+b1=55.9
+print("Before change")
+print(a1,b1)
+a1=float(a1)
+b1=int(b1)
+print("After change")
+print(a1,b1)
